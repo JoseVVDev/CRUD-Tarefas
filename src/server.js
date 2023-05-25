51333138f886c7cd002614ff45e5b87b731800c4
JoseVVDev <78755947+JoseVVDev@users.noreply.github.com>
2023-05-25 15:45:46 -0300

    criação do middleware de json e da classe database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,40 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
 
-const server = http.createServer((req, res) => {
-    return res.end('completo')
+const database = new Database()
+
+const server = http.createServer(async (req, res) => {
+
+    const { method, url } = req
+
+    await json(req, res)
+
+    if (method === 'POST' && url === '/tasks') {
+        const { title, description } = req.body
+
+        const task = {
+            id: randomUUID(),
+            title: title,
+            description: description,
+            created_at: new Date(),
+            completed_at: null,
+            updated_at: null
+        }
+
+        database.insert('tasks', task)
+        return res.writeHead(201).end()
+    }
+
+    if (method === 'GET' && url === '/tasks') {
+
+        const tasks = database.select('tasks')
+        return res
+            .end(JSON.stringify(tasks))
+    }
+
+    return res.writeHead(404).end('não encontrado')
 })
 
 server.listen(3335)
