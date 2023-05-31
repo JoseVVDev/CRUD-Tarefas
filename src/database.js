bcded123dc517c164e4a13542f7c71dcca483fcf
JoseVVDev <78755947+JoseVVDev@users.noreply.github.com>
2023-05-31 16:12:30 -0300

    metodo DELETE e parte do metodo PUT

--- src/database.js
+++ src/database.js
@@ -36,4 +36,24 @@ export class Database {
 
         return data
     }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(index => index.id === id)
+
+        if(rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
+
+        return rowIndex
+    }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(index => index.id === id)
+
+        if(rowIndex > -1) {
+            this.#database[table][rowIndex] = {id, ...data}
+            this.#persist()
+        }
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const tasks = database.select('tasks')
             return res.end(JSON.stringify(tasks))
@@ -14,7 +15,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const { title, description } = req.body
 
@@ -30,5 +31,39 @@ export const routes = [
             database.insert('tasks', task)
             return res.writeHead(201).end()
         }
-    }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params.groups
+
+            const deleteStatus = database.delete('tasks', id)
+
+            if (deleteStatus > -1) {
+                return res.writeHead(200).end()
+            }
+
+            return res.writeHead(404).end('provided ID was not found')
+
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params.groups
+            const { title, description } = req.body
+
+            const task = {
+                title: title,
+                description: description,
+                completed_at: null,
+                updated_at: new Date()
+            }
+
+            database.update('tasks', id, task)
+            return res.writeHead(200).end()
+        }
+    },
 ]
